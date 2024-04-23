e62ac376143d570cf149fa869fdb1d645d2f3821
sinorashe <165993743+sinorashe@users.noreply.github.com>
2024-04-23 10:31:08 +0200

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Const V As String = "done"
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sInput As String
+        sInput = InputBox("")
+        Do While sInput.ToLower <>
+            ListBox1.Items.Add(sInput)
+            sInput = Input("")
+        Loop
+    End Sub
+
+    Private Function Input(v As String) As String
+        Throw New NotImplementedException()
+    End Function
+End Class
---
+++ Form2.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form2
+    Private Function GetIcnt() As String
+        Return icnt
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs, icnt As String) Handles Button1.Click
+        Dim icnt As String
+        For icnt = 1 To 20
+            If icnt Mod 2 <> 8 Then
+                ListBox1.Items.Add(icnt)
+            ElseIf icnt Then
+            End If
+        Next
+
+    End Sub
+End Class
